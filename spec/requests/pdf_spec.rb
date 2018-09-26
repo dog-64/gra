c510732a2f7cc027d0d1046bab389a47798c111b
Oleg Duletsky <OlegDuletsky@Gmail.com>
2018-09-26 13:31:24 +0200

    fix - Pdf# - spec

--- spec/requests/pdf_spec.rb
+++ spec/requests/pdf_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Pdf', type: :request do
   describe 'show' do
     it 'not exist' do
       get pdf_path(0)
-      expect(response).to have_http_status(301)
+      expect(response).to have_http_status(302)
     end
     it 'ok' do
       cmtr = FactoryBot.create(:committer)
