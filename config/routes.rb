b5d194e21e545c58c3f952f35fd3d6ae19f0241d
Oleg Duletsky <OlegDuletsky@Gmail.com>
2018-09-27 11:07:13 +0200

    edit - Index#_form - method=post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'index#index'
   get '/url' => 'index#show'
-  get '/url/:url' => 'index#show', as: :url_id
+  post '/url' => 'index#show'
   get '/pdf/:id' => 'pdf#show', as: :pdf
   get '/zip/:id' => 'zip#show', as: :zip
 
